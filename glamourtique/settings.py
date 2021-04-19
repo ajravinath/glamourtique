1abc7b9eb528a3b01542830f833fbb34b146b429
Anuja <anuja@calcey.com>
2021-04-19 19:23:04 +0530

    minor fix

--- glamourtique/settings.py
+++ glamourtique/settings.py
@@ -102,10 +102,10 @@ DATABASES = {
 '''
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    # }
+    'default': {
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 db_from_env = dj_database_url.config(conn_max_age=500)
